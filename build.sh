ca9550463d4668c6c2a1605be7ddbba44de6ea6b
dirtyacc <dirtyaccount@126.com>
2014-01-25 09:05:31 -0500

    dirty-commit-first

---
+++ build.sh
@@ -0,0 +1,11 @@
+export ARCH=arm
+export CROSS_COMPILE=/home/randommm/Documents/linaro2/bin/arm-gnueabi-
+make -j4
+
+rm output/kernel/zImage -f
+cp arch/arm/boot/zImage output/kernel/
+cd output
+zip -r ../test.zip * -q
+
+
+
